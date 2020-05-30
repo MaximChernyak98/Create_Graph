d8b0770979cb3f5428dea6e8093d9d61732f677c
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2020-05-30 23:18:01 +0300

    Implemented the construction of two lines

--- Create_graphs.py
+++ Create_graphs.py
@@ -6,21 +6,43 @@ from datetime import datetime, date, time
 from openpyxl.chart import (LineChart, Reference, ScatterChart, Series, )
 from openpyxl.chart.axis import DateAxis
 
-graphs_book = load_workbook('C:\\Загрузки\14\\графики.xlsx')
+graphs_book = load_workbook('C:\\Work\\Python\\Create_graphs\\графики.xlsx')
 ws = graphs_book.active
+
+chart = ScatterChart()
+chart.title = "Scatter Chart"
+chart.style = 13
+chart.x_axis.title = 'Уровень воздействия, ед.'
+chart.y_axis.title = '2'
 
 
 class MLine:
+    num_x = 2
+    num_y = 2
+    line_name = ''
+    def __init__(self, x_column, y_column, worksheet):
+        self.x_column = x_column
+        self.y_column = y_column
+        while worksheet.cell(row=self.num_x, column=self.x_column).value is not None:
+            self.num_x += 1
+        while worksheet.cell(row=self.num_y, column=self. y_column).value is not None:
+            self.num_y += 1
+        self.line_name = str(worksheet.cell(row=1, column=self.x_column).value)
 
-    def __init__(self, x_column, y_column, name_column, ws):
+    def create_plot(self):
+        x_values = Reference(ws, min_col=self.x_column, min_row=2, max_row=self.num_x)
+        y_values = Reference(ws, min_col=self.y_column, min_row=2, max_row=self.num_y)
+        graph = Series(y_values, x_values, title=self.line_name)
+        graph.marker.symbol = 'triangle'
+        graph.marker.size = 5
+        return graph
 
-        # xvalues = Reference(ws, min_col=(type * 3 + 1), min_row=2, max_row=11)
-        # values = Reference(ws, min_col=(type * 3 + 2), min_row=1, max_row=11)
 
-        self.chart = ScatterChart()
-        self.chart.style = 13
-        self.chart.x_axis.title = 'Уровень воздействия, ед.'
-        self.chart.y_axis.title = str(ws.cell(row=1, column=y_index).value)
+first_line = MLine(x_column=1, y_column=4, worksheet=ws)
+second_line = MLine(x_column=10, y_column=13, worksheet=ws)
+chart.series.append(first_line.create_plot())
+chart.series.append(second_line.create_plot())
+ws.add_chart(chart, "A21")
 
 
 # class MGraph:
@@ -37,16 +59,5 @@ class MLine:
 
 
 
-for type in range(0, 33):
-    xvalues = Reference(ws, min_col=(type * 3 + 1), min_row=2, max_row=11)
-    values = Reference(ws, min_col=(type * 3 + 2), min_row=1, max_row=11)
-    graph = Series(values, xvalues, title_from_data=True)
-    graph.marker.symbol = 'triangle'
-    graph.marker.size = 5
-    print(graph.marker.symbol)
-
-    chart.series.append(graph)
-ws.add_chart(chart, "A12")
-
-ERI_dose_control.save('C:\\Work\\Python\\test2\\Для графика высвечивания.xlsx')
-ERI_dose_control.close()
+graphs_book.save('C:\\Work\\Python\\Create_graphs\\графики.xlsx')
+graphs_book.close()
